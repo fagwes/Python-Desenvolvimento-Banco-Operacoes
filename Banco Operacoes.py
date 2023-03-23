89faf33e859640a18f85b9a108577b57094b2b48
fagwes <109398136+fagwes@users.noreply.github.com>
2023-03-23 14:49:25 -0300

    Update Banco Operacoes.py

--- Banco Operacoes.py
+++ Banco Operacoes.py
@@ -31,3 +31,57 @@ elif loc == 'Transferencia':
 
 else:
     print('Digite uma Operação')
+    
+################################################################################################################################
+
+#EXEMPLO 2, ADICIONANDO UM INPUT PARA QUE O USUÁRIO INSIRA UM VALOR:
+
+loc = input('Escolha a Operação: ')
+valor = int(input('Digite o valor:'))
+
+Saldo = 1500
+Saque = valor
+Deposito = valor
+Transferencia = valor
+
+#SALDO#
+if loc == 'Saldo':
+    print('Saldo em Conta:',Saldo,)
+
+#SAQUE#
+elif loc == 'Saque':
+    print('Valor Solicitado de Saque:',valor,)
+    print('Operação Realizada com Sucesso:')
+    print('Saldo em Conta:',Saldo-valor)
+
+#DEPOSITO#
+
+elif loc == 'Deposito':
+    print('Valor de Deposito Realizado:',valor,)
+    print('Operação Realizada com Sucesso:')
+    print('Saldo em conta:',Saldo+valor)
+
+#TRANSFERENCIA#
+
+elif loc == 'Transferencia':
+    print('Valor de Transferencia Realizada:', valor, )
+    print('Operação Realizada com Sucesso:')
+    print('Saldo em conta:', Saldo - valor)
+
+else:
+    print('Digite uma operação')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
